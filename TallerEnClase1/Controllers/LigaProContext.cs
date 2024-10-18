f734657f71ed3f621ed34d5132bd2db9783f8252
Mateo Mikael Pillajo Guijarro <mateo.pillajo@udla.edu.ec>
2024-10-18 10:16:34 -0500

    Se relaizo la creacion de las clases requeridas para el proyecto.

--- TallerEnClase1/Controllers/LigaProContext.cs
+++ TallerEnClase1/Controllers/LigaProContext.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TallerEnClase1.Controllers;
-using TallerEnClase1.Models; // Asegúrate de usar el namespace correcto para los modelos
+using TallerEnClase1.Models; 
 
-namespace TallerEnClase1.Data // Cambia esto según donde esté ubicada la clase
+namespace TallerEnClase1.Data 
 {
     public class LigaProContext : DbContext
     {
@@ -10,7 +10,7 @@ namespace TallerEnClase1.Data // Cambia esto según donde esté ubicada la clase
         {
         }
 
-        // DbSet para mapear las clases de modelo a tablas de la base de datos
+        
         public DbSet<Estadio> Estadios { get; set; }
         public DbSet<Jugador> Jugadores { get; set; }
         public DbSet<Equipo> Equipos { get; set; }
